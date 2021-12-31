9fbf0b38297ad08a129cea287cf9cb2028d8a96a
Bioparco90 <monnimarco90@gmail.com>
2021-12-31 01:03:15 +0100

    creata nave giocatore
    gestita creazione e movimento nave giocatore e modifiche varie.

--- global.c
+++ global.c
@@ -1,10 +1,5 @@
 #include "global.h"
 
-/* Oggetto da rappresentare. Nave giocatore, nave nemica, oggetti di gioco quali siluri, proiettili, etc. */
-struct Object {
-    char identifier;    // Carattere dell'oggetto. Es: "-" come proiettile.
-    int x;              // Posizione dell'oggetto nell'asse x
-    int y;              // Posizione dell'oggetto nell'asse y
-};
+
 
 
--- global.h
+++ global.h
@@ -1,10 +1,21 @@
 #include <curses.h>
+#include <stdlib.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
 #define ENEMIES 25
+#define MAX_X 80
+#define MAX_Y 20
 
 void playerShip(int fd);
 void enemyShip(int fd);
 void gameArea(int fd);
 
 void enemiesGenerator(int *fd);
+
+/* Oggetto da rappresentare. Nave giocatore, nave nemica, oggetti di gioco quali siluri, proiettili, etc. */
+struct Object {
+    char identifier;    // Carattere dell'oggetto. Es: "-" come proiettile.
+    int x;              // Posizione dell'oggetto nell'asse x
+    int y;              // Posizione dell'oggetto nell'asse y
+};
