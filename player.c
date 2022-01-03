604db7064bc45a649d17cf4b7989459adc68b235
Bioparco90 <monnimarco90@gmail.com>
2022-01-03 15:17:20 +0100

    Prima bozza sparo giocatore
    Da completare, attualmente spara una sola volta

--- player.c
+++ player.c
@@ -2,6 +2,7 @@
 
 void playerShip(int fd){
     struct Object ship;
+    pid_t pidShot;
     
     ship.y = MAX_Y / 2;
     ship.x = 1;
@@ -23,9 +24,17 @@ void playerShip(int fd){
                     ship.y += 1;
                 break;
             
-            case ' ':
-                
-                rocket(fd);
+            case ' ':                
+                pidShot = fork();
+                if(pidShot == -1){
+                    printf("errore sparo");
+                } else{
+                    if (!pidShot){
+                        rocket(fd);
+                    } else{
+                        wait(NULL);
+                    }
+                }
                 // mvprintw(ship.y, ship.x+1, "o");
                 break;
 
@@ -42,7 +51,16 @@ void rocket(int fd){
     struct Object rocketUp;
     struct Object rocketDown;
 
+	rocketUp.x = 2;
+	rocketUp.y = MAX_Y / 2;
+
     write(fd, &rocketUp, sizeof(rocketUp));
 
-
+    while(rocketUp.x < MAX_X){
+		rocketUp.x++;
+		mvaddch(rocketUp.y, rocketUp.x-1, ' ');
+        mvprintw(rocketUp.y, rocketUp.x, "o");
+		refresh();
+		usleep(15000);
+    }
 }
