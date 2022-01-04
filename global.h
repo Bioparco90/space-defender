0652d9ba9f780f91796e07c0dc89610dbd441f40
Bioparco90 <monnimarco90@gmail.com>
2022-01-05 00:47:49 +0100

    Creazione nuove macro e aggiunta parametro alla struct

--- global.h
+++ global.h
@@ -14,6 +14,11 @@
 #define DIR_UP 1
 #define DIR_DOWN -1
 
+// Macro per la rappresentazione degli oggetti
+#define PLAYER "+"
+#define ENEMY "<"
+#define ROCKET "o"
+
 /* Oggetto da rappresentare. Nave giocatore, nave nemica, oggetti di gioco quali siluri, proiettili, etc. */
 struct Object {
     char identifier;    // Carattere dell'oggetto. Es: "-" come proiettile.
@@ -21,6 +26,7 @@ struct Object {
     int y;              // Posizione dell'oggetto nell'asse y
     int lives;          // Numero di vite disponibii dell'oggetto
     pid_t pid;          // Pid del processo di riferimento dell'oggetto
+    int firstShow;
 };
 
 // Funzioni libreria player.c
