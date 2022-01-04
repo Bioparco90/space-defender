d4d4e7777c1c9bf04cf540c11f07a2fc51c289ce
Bioparco90 <monnimarco90@gmail.com>
2022-01-04 19:45:34 +0100

    Aggiunta funzione generatore, da testare

--- global.c
+++ global.c
@@ -1,2 +1,18 @@
 #include "global.h"
 
+//globali
+int spawnX = MAX_X;
+int spawnY = 2;
+
+// codice non globale
+void generatore(int fd, struct Object *nemico){
+    nemico->x = spawnX;
+    nemico->y = spawnY;
+    nemico->lives = 2; // o quante sono
+    nemico->pid = getpid(); // siamo nel for delle fork in teoria
+    // ora possiamo modificare i valori
+    spawnY += 2 //spazio vuoto
+    if(nemico->y == MAX_Y - 1 || spawnY >= MAX_Y - 1)
+        spawnX -= 2; // spazio vuoto
+}
+// dovrebbe essere una roba del genere, senza manco usare il modulo
