4e5b7bf31b2077d92585619dc51d9dd09375ef9e
Bioparco90 <77575878+Bioparco90@users.noreply.github.com>
2022-01-05 18:37:35 +0100

    Nuovo main di prova

---
+++ main2.c
@@ -0,0 +1,51 @@
+#include "global.h"
+
+int main(){
+    struct Object enemy;
+    pid_t pidPlayerShip;         // Pid processo figlio "nave giocatore" 
+    pid_t pidEnemyShip[ENEMIES]; // Pid processo figlio "nave nemica"
+    int fd[2];                   // Descrittore pipe
+    int i;
+
+    initscr();         // Inizializza schermo di gioco
+    noecho();          // Disabilita visualizzazione tasti premuti
+    keypad(stdscr, 1); // Abilita tasti funzione (frecce)
+    curs_set(0);       // Disabilita visualizzazione cursore
+
+    // Creazione della pipe per la comunicazione tra i processi
+    pipe(fd);   
+
+    // Creazione primo processo figlio - Nave giocatore
+    switch (pidPlayerShip = fork()){
+        case -1:
+            printf("Errore creazione processo nave giocatore\n");
+            _exit(-1);
+
+        case 0:
+            close(fd[0]);       // Chiudiamo descrittore in lettura
+            playerShip(fd[1]);  // Gestiamo movimento giocatore passando descrittore in scrittura
+            _exit(0);
+    }
+
+    for(i=0; i<ENEMIES; i++){
+        switch(pidEnemyShip[i] = fork()){
+            case -1:
+                printf("Errore creazione processo nave nemica\n");
+                _exit(-1);
+
+            case 0:
+                close(fd[0]);
+                generatore(&enemy);
+                enemyShip(fd[1], enemy);
+                _exit(0);
+        }
+    }
+
+    close(fd[1]);
+    gameArea(fd[0]);
+
+    // int c = getch(); // di debug, da rimuovere poi
+    endwin();   // Ripristino del terminale
+
+    return 0;
+}
