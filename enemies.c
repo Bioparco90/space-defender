1081443a8b10f454eeef545936cb0b688b75ed84
Bioparco90 <monnimarco90@gmail.com>
2022-01-06 16:31:28 +0100

    Pulizia e spostamento funzione generatore1

--- enemies.c
+++ enemies.c
@@ -1,38 +1,24 @@
 #include "global.h"
 
-/* La funzione si occupa di generare le navi nemiche e di richiamare la funzione
-   che si occupa della rappresentazione della schermata di gioco.
-   Si è reso necessario optare per un if-else in modo da poter sfruttare la break 
-   nei processi padre, al fine di evitare la creazione di ^n processi nel ciclo. 
- */
-void enemiesGenerator(int *fd){
-    int pidEnemy[ENEMIES];
-    int enemyCounter;
+//globali
+int spawnX = MAX_X-1;
+int spawnY = 2;
 
+// codice non globale
+struct Object generatore(){
+    struct Object enemy;
+    enemy.x = spawnX;
+    enemy.y = spawnY;
+    enemy.identifier = ENEMY;
+    enemy.lives = 2; // o quante sono
+    enemy.pid = getpid(); // siamo nel for delle fork in teoria
+    // ora possiamo modificare i valori
+    spawnY += 2; //spazio vuoto
+    if(enemy.y == MAX_Y - 1 || spawnY >= MAX_Y - 1)
+        spawnX -= 2; // spazio vuoto
 
-    for(enemyCounter=0; enemyCounter<ENEMIES; enemyCounter++){
-        pidEnemy[enemyCounter] = fork();
-        if (pidEnemy[enemyCounter] == -1){
-            printf("Errore creazione processo nemico\n");
-            exit(2);
-        } else{
-            if (!pidEnemy[enemyCounter]){
-                struct Object enemy=generatore2(enemy,enemyCounter+1);
-                close(fd[0]);       // Chiudiamo descrittore in lettura
-                enemyShip(fd[1], enemy);   // Gestiamo movimento nemici passano descrittore in scrittura
-                close(fd[1]);
-                gameAreaV2(fd[0]);
-                _exit(0);
-            } //else{
-                //close(fd[1]);       // Chiudiamo descrittore in scrittura 
-                // gameArea(fd[0]);    // Gestiamo rappresentazione area di gioco passando descrittore in lettura
-                //gameAreaV2(fd[0]);
-               // break;
-            //}
-        }
-    }
+    return enemy;
 }
-
 
 struct Object generatore2(struct Object enemy, int enemyCounter){
 
