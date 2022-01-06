d1adbc05ca5fa7675bdd43ef835a9df8a861d90b
Bioparco90 <monnimarco90@gmail.com>
2022-01-06 17:16:18 +0100

    Adattamento main a tentativo V3 (bug)

--- main2.c
+++ main2.c
@@ -2,10 +2,10 @@
 
 int main(){
     struct Object enemy;
-    pid_t pidPlayerShip;         // Pid processo figlio "nave giocatore" 
-    pid_t pidEnemyShip[ENEMIES]; // Pid processo figlio "nave nemica"
-    int fd[2];                   // Descrittore pipe
-    int enemyPipe[ENEMIES][2];
+    pid_t pidPlayerShip;            // Pid processo figlio "nave giocatore" 
+    pid_t pidEnemyShip[ENEMIES];    // Pid processo figlio "nave nemica"
+    int fd[2];                      // Descrittore pipe generale
+    int allShipsPipe[ENEMIES+1][2]; // Descrittore pipe nave giocatore (0) e nemici
     int i;
 
     initscr();         // Inizializza schermo di gioco
@@ -17,8 +17,8 @@ int main(){
     pipe(fd);
 
     // Pipe dei nemici
-    for(i=0; i<ENEMIES; i++)
-        pipe(enemyPipe[i]);   
+    for(i=0; i<ENEMIES+1; i++)
+        pipe(allShipsPipe[i]);   
 
     // Creazione primo processo figlio - Nave giocatore
     switch (pidPlayerShip = fork()){
@@ -27,8 +27,8 @@ int main(){
             _exit(-1);
 
         case 0:
-            close(fd[0]);       // Chiudiamo descrittore in lettura
-            playerShip(fd[1]);  // Gestiamo movimento giocatore passando descrittore in scrittura
+            close(allShipsPipe[0][READ]);       // Chiudiamo descrittore in lettura
+            playerShip(allShipsPipe[0][WRITE]); // Gestiamo movimento giocatore passando descrittore in scrittura
             _exit(0);
     }
 
@@ -40,19 +40,20 @@ int main(){
 
             case 0:
                 // enemy = generatore2(enemy, i+1);
-                close(enemyPipe[i][0]);
+                close(allShipsPipe[i+1][READ]);
                 enemy = generatore();
                 enemy.serial = i;
-                enemyShip(enemyPipe[i][1], enemy);
+                enemyShip(allShipsPipe[i+1][WRITE], enemy);
                 _exit(0);
         }
     }
 
-    close(fd[1]);
+    // close(fd[1]);
     for (i=0; i<ENEMIES; i++){
-        close(enemyPipe[i][1]);
+        close(allShipsPipe[i][WRITE]);
     }
-    gameAreaV2(fd[0], enemyPipe);
+    gameAreaV3(allShipsPipe);
+    // gameAreaV2(fd[0], allShipsPipe);
 
     // int c = getch(); // di debug, da rimuovere poi
     endwin();   // Ripristino del terminale
