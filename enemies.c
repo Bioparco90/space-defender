e93f8e35e51c936869f75ef3be91377f1a7794f5
Giovanni Antonio Sprega <jonspre2001@gmail.com>
2022-01-08 13:41:24 +0100

    Non worka ma l'idea c'è, ho commentato spiegazione e possibile soluzione
    Aggiunto funzione enemyRocket

--- enemies.c
+++ enemies.c
@@ -25,7 +25,6 @@ struct Object generatore(int i){
 
 struct Object generatore2(int enemyCounter){
     struct Object enemy;
-
     enemy.y= 2* ((enemyCounter-1) % (MAX_ENEMY_COL))+2;//1 è il numero di spazi tra una nave e l'altra
     enemy.x= MAX_X - (((enemyCounter-1)/(MAX_ENEMY_COL))*2)-2;//1 è il numero di spazi tra una colonna di navi e l'altra
     enemy.identifier = ENEMY;
@@ -39,16 +38,77 @@ struct Object generatore2(int enemyCounter){
 void enemyShip(int mainPipe, int enemyPipe, struct Object enemy){
     int direction=1;
     char controlCharater;
+    pid_t pidEnemyRocket;
 
-    write(mainPipe, &enemy, sizeof(enemy));
-    while (true){
-        read(enemyPipe, &enemy, sizeof(enemy));
-        if(enemy.y < 2 || enemy.y > MAX_Y - 1) {
-            enemy.x -= 1;
-            direction *= -1;
-        }   
-        enemy.y += direction;
-        write(mainPipe, &enemy, sizeof(enemy));
-        usleep(500000);
+    //TL;DR stavo provando a testare la funzione enemyRocket in due maniere, in una il fork viene fatto direttamente all'interno della funzione e l'altro in enemyShip
+    //Lo switch qua dentro lo fa vedere, un fork è il processo del razzo l'altro fork è quello dei nemici. Non worka perché i cicli di movimento vanno in conflitto,
+    //bloccando le navi e lo sparo. Stessa cosa se facessi il fork in enemyRocket. Forse la soluzione potrebbe essere fare tanti processi razzo quanti sono i processi nemici ma non ho avuto tempo di testarla.
+
+   /* switch(pidEnemyRocket=fork()){
+        case -1:
+            printw("Errore generazione razzo");
+        case 0:
+            enemyRocket(mainPipe, enemyPipe);
+        default:
+            write(mainPipe, &enemy, sizeof(enemy));
+            while (true){
+                read(enemyPipe, &enemy, sizeof(enemy));
+                    if(enemy.y < 2 || enemy.y > MAX_Y - 1) {
+                        enemy.x -= 1;
+                        direction *= -1;
+                    }   
+                enemy.y += direction;
+                write(mainPipe, &enemy, sizeof(enemy));
+                usleep(500000);
+    }*/
+
+          write(mainPipe, &enemy, sizeof(enemy));
+            while (true){
+                read(enemyPipe, &enemy, sizeof(enemy));
+                    if(enemy.y < 2 || enemy.y > MAX_Y - 1) {
+                        enemy.x -= 1;
+                        direction *= -1;
+                    }   
+                enemy.y += direction;
+                write(mainPipe, &enemy, sizeof(enemy));
+                usleep(500000);
+    }
+  
+  
+}
+
+
+void enemyRocket(int mainPipe, int enemyPipe){
+    struct Object rocket;
+    struct Object enemy;
+    read(enemyPipe, &enemy, sizeof(enemy));
+    rocket.x=enemy.x+1;
+    rocket.y=enemy.y;
+    rocket.identifier=ENEMY_ROCKET;
+    rocket.serial=enemy.serial+ENEMIES;
+    rocket.lives=1;
+    rocket.pid=getpid();
+    
+    {
+
+        write(mainPipe,&rocket,sizeof(rocket));
+
+        while(true){
+           
+            if(random() < RAND_MAX/4){
+                do
+                {
+                    if(rocket.x <= 2){
+                        mvprintw(rocket.y,rocket.x," ");
+                        break;
+                    }
+                    rocket.x+=1;
+                } while (true);
+
+                 write(mainPipe, &rocket, sizeof(rocket));
+                  usleep(600000);
+            }
+
+        }
     }
 }
