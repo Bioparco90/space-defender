649568f0b9c52136effe335b85071c42ea2c6313
Bioparco90 <monnimarco90@gmail.com>
2022-01-10 17:09:15 +0100

    Fix spawn fuori schermo

--- enemies.c
+++ enemies.c
@@ -9,7 +9,7 @@ struct Object generator(int enemyCounter){
     struct Object enemy;
 
     enemy.y = 2* ((enemyCounter-1) % (MAX_ENEMY_COL))+2;      // 1 è il numero di spazi tra una nave e l'altra
-    enemy.x = MAX_X - (((enemyCounter-1)/(MAX_ENEMY_COL))-2); // 1 è il numero di spazi tra una colonna di navi e l'altra
+    enemy.x = MAX_X - ((enemyCounter-1)/(MAX_ENEMY_COL))-2; // 1 è il numero di spazi tra una colonna di navi e l'altra
     enemy.identifier = ENEMY; // Carattere identificativo dell'astronave nemica
     enemy.lives = 3; // lives = 3: nave di primo livello, lives < 3: nave di secondo livello
     enemy.pid = getpid();
@@ -22,10 +22,10 @@ void enemyShip(int mainPipe, int enemyPipe, struct Object enemy){
 
     write(mainPipe, &enemy, sizeof(enemy));         // Prima scrittura nella mainPipe
     while (true){                                   // Loop movimento nave
-        read(enemyPipe, &enemy, sizeof(enemy));     //Lettura dei dati provenienti dal loop di gioco
+        read(enemyPipe, &enemy, sizeof(enemy));     // Lettura dei dati provenienti dal loop di gioco
         if(enemy.y < 2 || enemy.y > MAX_Y - 1) {    // Controllo bordi
             enemy.x -= 1;       // Avanzamento di una colonna verso la nave del giocatore
-            direction *= -1;    //Cambio direzione 
+            direction *= -1;    // Cambio direzione 
         }   
         enemy.y += direction;   // Nuova coordinata Y sfruttando la direzione
         write(mainPipe, &enemy, sizeof(enemy)); // Scrittura ciclica nella mainPipe
