5ad02443c001542a36f5ec1d5a280c045c638017
Giovanni Antonio Sprega <jonspre2001@gmail.com>
2022-01-14 19:53:54 +0100

    messo srand nel main

--- main.c
+++ main.c
@@ -6,6 +6,7 @@ int main(){
     pid_t pidPlayerShip;            // Pid processo figlio "nave giocatore" 
     pid_t pidEnemyFleet;
     int mainPipe[2];                // Pipe generale
+    srand((unsigned) time(NULL) );
 
     initscr();         // Inizializza schermo di gioco
     noecho();          // Disabilita visualizzazione tasti premuti
