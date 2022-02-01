99f3961703d7909118691744099fca74b46a9281
Bioparco90 <monnimarco90@gmail.com>
2022-02-02 00:04:26 +0100

    Leggere descrizione
    Tutto il source è stato riadattato ai threads.  Per i thread degli spari è stato necessario utilizzare degli array in quanto utilizzando le semplici variabili si andava incontro a pesantissimi glitch grafici, cambi di velocità e di moto dei razzi e altrettanto pesanti insulti alle divinità.

--- thread/player.c
+++ thread/player.c
@@ -1,49 +1,47 @@
 #include "global.h"
 
 // Funzione per l'inizializzazione e la gestione delle azioni della nave del giocatore
-void playerShip(int mainPipe){
+void* playerShip(){
     struct timespec time, checker;
-    Object ship;
+    // Object ship;
     int serial;
 
     clock_gettime(CLOCK_REALTIME, &time);   // Rilevazione iniziale del tempo
     
     // Inizializzazione nave giocatore
-    ship.x = 2;
-    ship.y = MAX_Y / 2;
-    ship.identifier = PLAYER;
-    ship.pid = getpid();
-    ship.serial = 777;
+    player.x = 2;
+    player.y = MAX_Y / 2;
+    player.identifier = PLAYER;
+    player.serial = 777;
     
-    write(mainPipe, &ship, sizeof(ship)); // Prima scrittura nella mainPipe
     serial = 0;                         // Inizializzazione serial razzi giocatore
 
     // Ciclo movimento e sparo
-    while(true){
+    while(player.lives && enemyCounter){
         int c = getch();
 
         switch (c){
             case KEY_UP:                // Movimento verso l'alto
-                if(ship.y > 2)          // Verifica collisione bordo
-                    ship.y -= 1;
+                if(player.y > 2)          // Verifica collisione bordo
+                    player.y -= 1;
                 break;
             
             case KEY_DOWN:              // Movimento verso il basso
-                if (ship.y < MAX_Y-1)   // Verifica collisione bordo
-                    ship.y += 1;
+                if (player.y < MAX_Y-1)   // Verifica collisione bordo
+                    player.y += 1;
                 break;
 
             case ' ':                                                   // Sparo alla pressione del tasto "spazio"
                 clock_gettime(CLOCK_REALTIME, &checker);                // Rilevazione del tempo trascorso dalla precedente rilevazione
                 if (serial == 0){                                       // Primo sparo della partita
                     system("aplay sounds/laser.wav 2> /dev/null &");    // Riproduce suono sparo
-                    playerShotInit(mainPipe, ship.x, ship.y, serial);     // Generazione sparo
+                    playerShotInit(player.x, player.y, serial);     // Generazione sparo
                     serial++;                                           // Incremento del serial (verrà utilizzato come indice nella funzione gestore di gioco)
                     time = checker;                                     // Il valore dell'ultima rilevazione viene salvato come "rilevazione precedente"
                 }
                 if (checker.tv_sec - time.tv_sec >= 1){                 // Spari successivi al primo. Valuta se sia passato ALMENO un secondo dallo sparo precedente
                     system("aplay sounds/laser.wav 2> /dev/null &");    // Riproduce suono sparo
-                    playerShotInit(mainPipe, ship.x, ship.y, serial);     // Generazione sparo
+                    playerShotInit(player.x, player.y, serial);     // Generazione sparo
                     serial++;                                           // Incremento del serial (verrà utilizzato come indice nel gameLoop)
                     time = checker;                                     // Il valore dell'ultima rilevazione viene salvato come "rilevazione precedente"
                 }
@@ -51,66 +49,78 @@ void playerShip(int mainPipe){
         }
         if(serial >= MAX_ROCKET)            // Verifica se sia stato raggiunto il limite di spari esistenti in uno stesso momento
             serial = 0;                     // L'indicizzazione riparte da 0
-        write(mainPipe, &ship, sizeof(ship)); // Scrittura ciclica nella mainPipe passata al loop di gioco
     }
 }
 
 // Funzione per l'inizializzazione degli spari diagonali
-void playerShotInit(int mainPipe, int x, int y, int serial){
-    pid_t pidRocketUp, pidRocketDown;
+void playerShotInit(int x, int y, int serial){
+    pthread_t thRocketUp[MAX_ROCKET], thRocketDown[MAX_ROCKET];
 
-    pidRocketUp = fork();                                       // Inizializzazione processo razzo diagonale superiore
-    switch (pidRocketUp){                                       
-        case -1:                                                // Fallimento fork
-            _exit(-1);                                          // Gestione errore
-            break;
+    ShotArgs argsUp = {x, y, DIR_UP, serial};
+    ShotArgs argsDown = {x, y, DIR_DOWN, serial};
 
-        case 0:                                                 // Processo razzo diagonale superiore
-            shot(mainPipe, x, y, DIR_UP, serial);               // Inizializzazione e gestore movimento razzo
-            _exit(0);                                           // Chiusura processo
-            break;
+    if (pthread_create(&thRocketUp[serial], NULL, &shotUp, &argsUp)){
+        endwin();
+        exit;
+    }
 
-        default:                                                // Processo padre
-            pidRocketDown = fork();                             // Inizializzazione processo razzo diagonale inferiore
-            switch (pidRocketDown){
-                case -1:                                        // Fallimento fork
-                    _exit(-1);                                  // Gestione errore
-                    break;
-
-                case 0:                                         // Processo razzo diagonale inferiore
-                    shot(mainPipe, x, y, DIR_DOWN, serial);     // Inizializzazione e gestore movimento razzo
-                    _exit(0);                                   // Chiusura processo
-                    break;
-            }
+    if (pthread_create(&thRocketDown[serial], NULL, &shotDown, &argsDown)){
+        endwin();
+        exit;
     }
 }
 
 // Funzione per la gestione del movimento del singolo razzo
-void shot(int mainPipe, int x, int y, int direction, int serial){
-    Object rocket;
+void* shotUp(void* param){
+    ShotArgs* args;
+    int id;
+    int direction;
+    
+    args = (ShotArgs*) param;
 
-    // Inizializzazione razzo
-    if(direction == DIR_UP)                 // Valutazione direzione sparo
-        rocket.identifier = ROCKET_UP;      // Razzo giocatore alto
-    else
-        rocket.identifier = ROCKET_DOWN;    // Razzo giocatore basso
+    id = args->serial;
+    direction = args->dir;
 
-    rocket.x = x + 2;
-   	rocket.y = y + 1 + direction;
-    rocket.lives = 1;
-    rocket.pid = getpid();
-    rocket.serial = serial;
-
-    write(mainPipe, &rocket, sizeof(rocket));       // Prima scrittura nella mainPipe
+    rocketUp[id].x = args->x + 2;
+   	rocketUp[id].y = args->y + 1 + args->dir;
+    rocketUp[id].lives = 1;
+    rocketUp[id].serial = args->serial;
 
     // Ciclo movimento razzo
-    while(true){
-        if((rocket.y < 2 || rocket.y > MAX_Y)){     // Verifica collisione bordi
-            direction *= -1;                        // Cambio direzione
+    while(rocketUp[id].lives && rocketUp[id].x < MAX_X){
+        if((rocketUp[id].y < 2 || rocketUp[id].y > MAX_Y)){     // Verifica collisione bordi
+            direction *= -1;                                    // Cambio direzione
         }
-        rocket.x += 1;                              // Spostamento orizzontale
-        rocket.y += direction;                      // Spostamento verticale, direzione dipendente dal valore di "direction"
-        write(mainPipe, &rocket, sizeof(rocket));   // Scrittura ciclica nella mainPipe passata al loop di gioco
-		usleep(ROCKET_DELAY);                       // Delay movimento razzo
+        rocketUp[id].x += 1;                                    // Spostamento orizzontale
+        rocketUp[id].y += direction;                            // Spostamento verticale, direzione dipendente dal valore di "direction"
+
+		usleep(ROCKET_DELAY);                                   // Delay movimento razzo
+    }
+}
+
+void* shotDown(void* param){
+    ShotArgs* args;
+    int id;
+    int direction;
+    
+    args = (ShotArgs*) param;
+
+    id = args->serial;
+    direction = args->dir;
+
+    rocketDown[id].x = args->x + 2;
+   	rocketDown[id].y = args->y + 1 + args->dir;
+    rocketDown[id].lives = 1;
+    rocketDown[id].serial = args->serial;
+
+    // Ciclo movimento razzo
+    while(rocketDown[id].lives && rocketDown[id].x < MAX_X){
+        if((rocketDown[id].y < 2 || rocketDown[id].y > MAX_Y)){     // Verifica collisione bordi
+            direction *= -1;                                        // Cambio direzione
+        }
+        rocketDown[id].x += 1;                                      // Spostamento orizzontale
+        rocketDown[id].y += direction;                              // Spostamento verticale, direzione dipendente dal valore di "direction"
+
+		usleep(ROCKET_DELAY);                                       // Delay movimento razzo
     }
 }
