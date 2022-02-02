15b891800838575df586e4af9b7f62ab37afebf1
Bioparco90 <monnimarco90@gmail.com>
2022-02-02 22:32:59 +0100

    Adattamento nemici e razzi nemici ai thread

--- thread/enemies.c
+++ thread/enemies.c
@@ -1,68 +1,65 @@
 #include "global.h"
+#include <curses.h>
 
 // Funzione per l'inizializzazione, la generazione e il posizionamento iniziale delle navi nemiche.
 void fleetEnlister(int mainPipe){
-    Object enemy[ENEMIES];
-    pid_t pidEnemyShip[ENEMIES];
+    pthread_t thEnemyShip[ENEMIES];
+    Args arg;
     int posX = MAX_X - 2;
     int posY = 2;
     int i;
-    
-    // Ciclo per la generazione dei processi di tutte le navi nemiche e la relativa inizializzazione
-    for(i=0; i<ENEMIES; i++){
-        pidEnemyShip[i] = fork();
-        switch(pidEnemyShip[i]){
-            case -1:
-                endwin();
-                printf("Errore creazione processo nemico #%d", i);
-                exit(1);
 
-            case 0:
-                // Inizializzazione singola nave
-                enemy[i].x = posX;
-                enemy[i].y = posY;
-                enemy[i].pid = getpid();
-                enemy[i].serial = i;
-                enemy[i].identifier = ENEMY;
-                enemyShip(mainPipe, enemy[i]);  // Chiamata a gestore movimento nave
+    for (i=0; i<ENEMIES; i++){
+        arg.x = posX;
+        arg.y = posY;
+        arg.serial = i;
+        if (pthread_create(&thEnemyShip[i], NULL, &enemyShip, &arg)){
+            endwin();
+            exit(1);
         }
-        posY += 4;              // Posizione asse Y della nave successiva
-        if(posY >= MAX_Y -1) {  // Controllo posizionamento massimo asse Y
-            posX -= 8;          // Prossima colonna di navi
-            posY = 2;           // Prima posizione della colonna (nave di testa)
+        posX += 4;
+        if (posY >= MAX_Y - 1){
+            posX -= 8;
+            posY = 2;
         }
     }
-
-    // Ciclo per l'attesa della terminazione di tutti i processi figli (navi)
-    for (i=0; i<ENEMIES; i++)
-        waitpid(enemy[i].pid, NULL, 0);
 }
 
 // Funzione per la gestione delle azioni della singola nave nemica
-void enemyShip(int mainPipe,  Object enemy){
+void* enemyShip(void* param){
     int direction = 1;              // Direzione nave: 1 -> basso, -1 -> alto
     int flag = VERTICAL;            // Flag da sfruttare per gestire il movimento verticale
     struct timespec start, end;     // Utilizzate per scandire il tempo minimo tra uno sparo e l'altro
     struct timespec start2, end2;   // Utilizzate come countdown per il cambio di velocità del movimento nemico
     int timeTravel;                 // Tempo massimo di percorrenza dell'area di gioco
     int delay = ENEMY_DELAY;        // Ritardo movimento navi
+    int id;
+
+    pthread_t thEnemyShot;
+    Args shotArg;
+    Args* arg;
+    arg = (Args*) param;
+
+    id = arg->serial;
+    enemy[id].x = arg->x;
+    enemy[id].y = arg->y;
+    enemy[id].serial = arg->serial;
 
     // Rilevazioni iniziali del tempo
     clock_gettime(CLOCK_REALTIME, &start);                  // Rilevazione per il tempo degli spari
     clock_gettime(CLOCK_REALTIME, &start2);                 // Rilevazione per l'incremento della velocità delle navi
     timeTravel = timeTravelEnemyRocket(ROCKET_DELAY);       // Valutazione tempo massimo di percorrenza di uno sparo
 
-    write(mainPipe, &enemy, sizeof(enemy));                 // Prima scrittura nella mainPipe
-    while(true){                                            // Loop movimento nave nemica
+    while(enemy[id].lives){ // Loop movimento nave nemica
         switch (flag){
             case VERTICAL:                                  // Movimento verticale
-                enemy.y += direction;                       // Aggiornamento coordinata Y
-                if (enemy.y <= 2 || enemy.y > MAX_Y - 1)    // Verifica collisione bordi
+                enemy[id].y += direction;                       // Aggiornamento coordinata Y
+                if (enemy[id].y <= 2 || enemy[id].y > MAX_Y - 1)    // Verifica collisione bordi
                     flag = HORIZONTAL;                      // Eventuale modifica del valore flag, che ci manderà al movimento orizzontale
                 break;
 
             case HORIZONTAL:        // Movimento orizzontale
-                enemy.x -= 4;       // Aggiornamento coordinata X
+                enemy[id].x -= 4;   // Aggiornamento coordinata X
                 direction *= -1;    // Rimbalzo sul bordo
                 flag = VERTICAL;    // Settiamo la flag per tornare al movimento verticale
                 break;
@@ -71,8 +68,14 @@ void enemyShip(int mainPipe,  Object enemy){
         // Sparo nemico 
         clock_gettime(CLOCK_REALTIME, &end);                            // Rilevazione del tempo trascorso dalla precedente rilevazione (spari)
         if (end.tv_sec - start.tv_sec >= timeTravel + 1){               // Controllo sul tempo trascorso dall'ultimo sparo
-            if (rand()%ENEMIES == enemy.serial){                        // Generazione numero casuale. Se uguale al serial della nave, può sparare
-                enemyShot(mainPipe, enemy.x, enemy.y, enemy.serial);    // Chiamata a generazione sparo
+            if (rand()%ENEMIES == enemy[id].serial){                        // Generazione numero casuale. Se uguale al serial della nave, può sparare
+                shotArg.x = enemy[id].x;
+                shotArg.y = enemy[id].y;
+                shotArg.serial = enemy[id].serial;
+                if (pthread_create(&thEnemyShot, NULL, &enemyShot, &shotArg)){
+                    endwin();
+                    exit(1);
+                }
                 start = end;                                            // Il valore dell'ultima rilevazione viene salvato come "rilevazione precedente"
             }
         }
@@ -82,37 +85,27 @@ void enemyShip(int mainPipe,  Object enemy){
             delay *= 0.8;                           // Incremento della velocità di movimento: 20%
             start2 = end2;                          // Il valore dell'ultima rilevazione viene salvato come "rilevazione precedente"
         }
-        write(mainPipe, &enemy, sizeof(enemy));     // Scrittura ciclica nella mainPipe passata al loop di gioco
         usleep(delay);                              // Ritardo movimento navi
     }
+    return NULL;
 }
 
 // Funzione per la gestione del movimento del razzo nemico
-void enemyShot(int mainPipe, int x, int y, int serial){
-    pid_t pidEnemyShot;
-    Object shot;
-
-    pidEnemyShot = fork();  // Creazione processo per il razzo nemico
-    switch (pidEnemyShot){
-        case -1:
-            endwin();
-            printf("Errore creazione missile nemico\n");
-            exit(1);
+void* enemyShot(void* param){
+    Args* arg;
+    int id;
     
-        case 0:
-            // Inizializzazione razzo
-            shot.x = x - 2;
-            shot.y = y + 1;
-            shot.identifier = ENEMY_ROCKET;
-            shot.pid = getpid();
-            shot.serial = serial;
+    arg = (Args*) param;
+    id = arg->serial;
 
-            write(mainPipe, &shot, sizeof(shot));       // Prima scrittura nella mainPipe
-            while (true){                               // Loop movimento razzo
-                shot.x -= 1;                            // Movimento orizzontale
-                write(mainPipe, &shot, sizeof(shot));   // Scrittura ciclica nella mainPipe passata al loop di gioco
-                usleep(ROCKET_DELAY);                   // Ritardo movimento
-            }
-            _exit(0);                                   // Chiusura processo
+    // Inizializzazione razzo
+    enemyRocket[id].x = arg->x - 2;
+    enemyRocket[id].y = arg->y + 1;
+    enemyRocket[id].serial = arg->serial;
+
+    while (enemyRocket[id].lives && enemyRocket[id].x > 0){                               // Loop movimento razzo
+        enemyRocket[id].x -= 1;                            // Movimento orizzontale
+        usleep(ROCKET_DELAY);                   // Ritardo movimento
     }
+    return NULL;
 }
